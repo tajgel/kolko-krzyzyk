ae00649d85d612c55873ded328139991dda045fc
Miłosz <tajgelyt@gmail.com>
2025-01-17 15:17:55 +0100

    added draw check

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,56 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+type Square = {
+  id: string;
+  clicked: boolean;
+  text: string;
+};
 
 function App() {
-  type squaresType = {
-    id: string,
-    clicked: boolean,
-    text: string
-  }
-
-  const tempSquares = Array.from({ length: 9 }, (_, i) => ({
+  const tempSquares: Square[] = Array.from({ length: 9 }, (_, i) => ({
     id: i.toString(),
     clicked: false,
-    text: ""
-  }))
-  const[squares, setSquares] = useState<Array<squaresType>>(tempSquares)
-  const[checked, setChecked] = useState<boolean>(false)
+    text: "",
+  }));
 
-  function handleReset(){
-    setSquares(tempSquares)
-    setChecked(false)
+  const [squares, setSquares] = useState<Square[]>(tempSquares);
+  const [squaresContent, setSquaresContent] = useState<string[]>()
+  const [checked, setChecked] = useState<boolean>(false);
+
+  function resetSquares() {
+    setSquares(tempSquares);
+    setChecked(false);
   }
 
-  function handleClick(event: React.MouseEvent){
-    let tempArray = squares.map((element) => {
-      if (element.id === event.currentTarget.id) {
-        if (element.clicked) {
-          alert("Nie można kliknąć 2 razy tego samego");
-          return element;
-        } else {
-          return { ...element, clicked: true, text: checked ? "X" : "O" };
-        }
-      } else {
-        return { ...element };
-    }})
-    setSquares(tempArray)
-    setChecked(!checked)
-    let winner = checkForWinner(tempArray)
-    if(winner?.text === "O"){
-      alert("Wygrał O")
-    }
-    else if(winner?.text === "X"){
-      alert("wygrał X")
-    }
-    // let lol = tempArray.map((element) => {
-    //   if(element.clicked === true){
-    //     pierd+=1;
-    //   }
-    // })
-    // if(pierd !== 0){
+  function handleClick(event: React.MouseEvent) {
+    const target = event.currentTarget as HTMLDivElement
+    const squareId = target.dataset.id;
+    const selectedSquare = squares.find((square) => square.id === squareId);
 
-    // }
+    if (!selectedSquare || selectedSquare.clicked) {
+      alert("Nie można kliknąć 2 razy tego samego");
+      return;
+    }
+
+    const updatedSquares = squares.map((square) =>
+      square.id === squareId
+        ? { ...square, clicked: true, text: checked ? "X" : "O" }
+        : square
+    );
+    setSquaresContent(updatedSquares.map((element) => element.text))
+    console.log(squaresContent)
+    setSquares(updatedSquares);
+    setChecked(!checked);
+
+    const winner = checkForWinner(updatedSquares);
+    if (winner) {
+      alert(`Wygrał ${winner.text}`);
+      resetSquares()
+    }
   }
 
-  function checkForWinner(currentPositions: Array<squaresType>){
-    let positions = currentPositions.map((element) => element.text)
-    console.log(positions)
+  function checkForWinner(squares: Square[]) {
     const winningPositions = [
       ["0", "1", "2"],
       ["3", "4", "5"],
@@ -63,27 +59,51 @@ function App() {
       ["1", "4", "7"],
       ["2", "5", "8"],
       ["0", "4", "8"],
-      ["2", "4", "6"]
-    ]
-    for(let i=0; i<winningPositions.length; i++){
-      const [a, b, c] = winningPositions[i]
-      if(currentPositions[Number(a)].text && currentPositions[Number(a)].text === currentPositions[Number(b)].text && currentPositions[Number(a)].text === currentPositions[Number(c)].text){
-        return currentPositions[Number(a)]
+      ["2", "4", "6"],
+    ];
+    
+    for (const [a, b, c] of winningPositions) {
+      if (
+        squares[+a].text &&
+        squares[+a].text === squares[+b].text &&
+        squares[+a].text === squares[+c].text
+      ) {
+        return squares[+a];
       }
     }
-
+    let y = 0;
+    for(let i = 0; i<squares.length; i++){
+      if(squares[i].clicked){
+        y+=1
+      }
+      else{
+        continue
+      }
+    }
+    if(y===9){
+      alert("Draw")
+      return null
+    }
+    return null;
   }
 
   return (
-    <div id='App'>
-      <button onClick={handleReset}>reset</button>
-      <div id='siatka'>
+    <div id="App">
+      <button onClick={resetSquares}>Reset</button>
+      <div id="siatka">
         {squares.map((square) => (
-          <div id={square.id} className='square' onClick={handleClick}>{square.text}</div>
+          <div
+            key={square.id}
+            data-id={square.id}
+            className="square"
+            onClick={handleClick}
+          >
+            {square.text}
+          </div>
         ))}
-      </div>  
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
